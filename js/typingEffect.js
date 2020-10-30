f95ee29f18d0a7cd6609ae777f098c36428f4ce1
barlydjaja <barlydjaja@gmail.com>
2020-10-31 00:06:03 +0700

    initial commit

---
+++ js/typingEffect.js
@@ -0,0 +1,15 @@
+let typed = new Typed(".typing", {
+  strings: ["WELCOME", "Enjoy The Page"],
+  startDelay: 500,
+  typeSpeed: 100,
+  loop: true,
+});
+
+let kedua = new Typed(".kedua", {
+  strings: [
+    "WEBSITE INI ADALAH CONTOH LANDING PAGE UNTUK TUGAS-6 BOOTCAMP GLINTSxBINAR",
+  ],
+  startDelay: 4200,
+  typeSpeed: 40,
+  loop: false,
+});
